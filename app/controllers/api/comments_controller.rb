1270d9b235448f80a43c12758a18ba0bb31aa4ad
James Yeates <jimibue@gmail.com>
2020-03-17 16:39:32 -0600

    create comments

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -4,7 +4,18 @@ class Api::CommentsController < ApplicationController
     render json: product.comments
   end
 
+  def create
+    product = Product.find(params[:product_id])
+    render json: product.comments.create(comment_params)
+  end
+
   def destroy
     Comment.find(params[:id]).destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:user, :text)
   end
 end
--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import axios from "axios";
-import { Segment, Header, Button } from "semantic-ui-react";
+import { Segment, Header, Button, Form } from "semantic-ui-react";
 
 export default class Comments extends Component {
-  state = { comments: [] };
+  state = { comments: [], user:'', text:'' };
   componentDidMount() {
     const { productId } = this.props;
     axios.get(`/api/products/${productId}/comments`).then(res => {
@@ -24,9 +24,53 @@ export default class Comments extends Component {
     })
 
   }
+  handleChange =(e)=>{
+    const name = e.target.name
+    const value = e.target.value
+    this.setState({
+      [name]: value
+    })
+  }
+  handleSubmit=(e)=>{
+    const { productId } = this.props;
+    axios.post(`/api/products/${productId}/comments`,{
+      user: this.state.user,
+      text:this.state.text
+    }).then((res)=>{
+      this.setState({
+        comments:[res.data, ...this.state.comments]
+      })
+    }).catch(err=>{
+
+    })
+    // this.setState({
+    //   [name]: value
+    // })
+  }
   render() {
+    const { user,  text} = this.state
     return (
       <Segment>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group widths="equal">
+            <Form.Input
+              label="User"
+              name="user"
+              placeholder="User"
+              value={user}
+              onChange={this.handleChange}
+              required
+            />
+            <Form.Input
+              label="Add Comment"
+              name="text"
+              placeholder="Comment"
+              value={text}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Button color="blue">Submit</Form.Button>
+        </Form>
         <Header as="h1">Comments 2nd example</Header>
         {this.state.comments.map(comment => (
           <Segment key={`comment=${comment.id}`}>
